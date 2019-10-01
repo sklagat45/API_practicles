3e2cfe1402444bc80b0337dca47c0fa5cedad2fa
Samuel Kiplagat <49307079+sklagat45@users.noreply.github.com>
2019-10-01 23:03:46 +0300

    Add files via upload

---
+++ api/objects/product.php
@@ -0,0 +1,171 @@
+<?php
+class Product{
+
+    // database connection and table name
+    private $conn;
+    private $table_name = "products";
+
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $created;
+    // read products
+    // update the product
+function update(){
+
+    // update query
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                price = :price,
+                description = :description,
+                category_id = :category_id
+            WHERE
+                id = :id";
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->price=htmlspecialchars(strip_tags($this->price));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->id=htmlspecialchars(strip_tags($this->id));
+
+    // bind new values
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':price', $this->price);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+
+    return false;
+}
+function read(){
+
+    // select all query
+    $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            ORDER BY
+                p.created DESC";
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    // execute query
+    $stmt->execute();
+
+    return $stmt;
+}
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // delete the product
+function delete(){
+
+    // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+
+    return false;
+
+}
+// search products
+function search($keywords){
+
+    // select all query
+    $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            WHERE
+                p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ?
+            ORDER BY
+                p.created DESC";
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $keywords=htmlspecialchars(strip_tags($keywords));
+    $keywords = "%{$keywords}%";
+
+    // bind
+    $stmt->bindParam(1, $keywords);
+    $stmt->bindParam(2, $keywords);
+    $stmt->bindParam(3, $keywords);
+
+    // execute query
+    $stmt->execute();
+
+    return $stmt;
+}
+// create product
+function create(){
+
+    // query to insert record
+    $query = "INSERT INTO
+                " . $this->table_name . "
+            SET
+                name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->price=htmlspecialchars(strip_tags($this->price));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->created=htmlspecialchars(strip_tags($this->created));
+
+    // bind values
+    $stmt->bindParam(":name", $this->name);
+    $stmt->bindParam(":price", $this->price);
+    $stmt->bindParam(":description", $this->description);
+    $stmt->bindParam(":category_id", $this->category_id);
+    $stmt->bindParam(":created", $this->created);
+
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+
+    return false;
+
+}
+}
---
+++ api/readme.txt
@@ -0,0 +1,4 @@
+http://localhost/api/objects/search.php?s=watch
+http://localhost/api/objects/delete.php?s=watch
+http://localhost/api/objects/create.php?s=watch
+http://localhost/api/objects/update.php?s=watch
